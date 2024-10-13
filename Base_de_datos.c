a33399f9a2fab6330a54e4b92d56355132ca0013
carlos5213 <142379396+carlos5213@users.noreply.github.com>
2024-10-13 03:32:24 +0000

    Cambios

--- Base_de_datos.c
+++ Base_de_datos.c
@@ -1,7 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct nodo
+{
+  char nombre[30];
+  int edad;
+  char sexo;
+  float altura;
+  double peso;
+  struct nodo *sig;
+}GEN;
+
+
+int asignaMem(GEN **ptr, int n){
+ int res = 0, cont;
+ *ptr = (GEN *)malloc(sizeof(GEN));
+if(*ptr)
+{
+  res = 1;
+  for(cont = 0; cont < n && res; cont++)
+    {
+      printf("Escriba datos del paciente:\n");
+      printf("Nombre:\n");
+      printf("Edad:\n");
+      printf("Sexo:\n");
+      printf("Altura:\n");
+      printf("Peso:\n");
+    }
+}
+}
+
+int creaNodo(GEN *nuevo, char nombre[], int edad, char sexo, float altura, double peso)
+{
+  
+}
+
 
 int main()
 {
-    printf("Hello wordl");
+  int tambloque = sizeof(struct nodo);
+  void *ptrGen;
 }
